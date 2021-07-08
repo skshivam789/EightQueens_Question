f6127c4ee7d4d80cd2cfac3a9e926af2ff5fc8b8
Admin <Admin@Asus>
2021-07-08 17:18:54 +0530

    initial commit

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -2,7 +2,12 @@ public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
 
 		// WRITE YOUR LOGIC HERE...
-		System.out.println(square);
-		return true;
+		int a = square/8;
+		int b = square%8;
+		if((a%2==0 && b%2!=0)||(a%2!=0 && b%2==0))
+			return true;
+		else
+			return false;		
+		
 	}
 }
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -4,14 +4,14 @@ public class GameStrategy {
 
 	private int getColumn(int cellId) {
 		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+        int col = cellId%8;
+		return col;	// just for the heck of it
 	}
 	
 	private int getRow(int cellId) {
 		// WRITE YOUR LOGIC HERE....................................
-		
-		return 0;	// just for the heck of it
+		int row = cellId/8;
+		return row;	// just for the heck of it
 	}
 
 	public boolean isValidPosition(int cellId) {
@@ -29,6 +29,71 @@ public class GameStrategy {
 			WRITE YOUR LOGIC HERE...............................
 
 		*/
+		// checking in the same row
+		for(int i=0;i<=7;i++) {
+			if (i!=col && placedQueens[row][i]==true) {
+				isValid = false;
+				break;
+			}
+			}
+		// checking in the same column
+		if(isValid==true) {
+		for(int i=0;i<=7;i++) {
+			if (i!=row && placedQueens[i][col]==true) {
+				isValid = false;
+				break;
+			}
+		}
+		}
+		// checking along the diagonal positions
+		int i =row, j=col;
+		if(isValid==true) {
+		while(i<7&&j<7) {
+			i+=1;
+			j+=1;
+			if (placedQueens[i][j]==true) {
+				isValid = false;
+				break;
+			}
+		}
+		}
+		if(isValid==true) {
+		i =row; j=col;
+		while(i>0&&j>0) {
+			i-=1;
+			j-=1;
+			if (placedQueens[i][j]==true) {
+				isValid = false;
+				break;
+			}
+		}
+		}
+		if(isValid==true) {
+		i =row; j=col;
+		while(i<7&&j>0) {
+			i+=1;
+			j-=1;
+			if (placedQueens[i][j]==true) {
+				isValid = false;
+				break;
+			}
+		}
+		}
+		if(isValid==true) {
+		i =row; j=col;
+		while(i>0&&j<7) {
+			i-=1;
+			j+=1;
+			if (placedQueens[i][j]==true) {
+				isValid = false;
+				break;
+			}
+		}
+		}
+		if (isValid==true) {
+			placedQueens[row][col]=true;
+			numQueens+=1;
+		}
 		return isValid;
 	}
 }
